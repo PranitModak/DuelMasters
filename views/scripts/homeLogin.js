448aaa16fefd6ea8bf14af5dd61a43d11bc13988
PranitModak <empranit@gmail.com>
2019-05-26 02:39:19 +0530

    Update homeLogin.js

--- views/scripts/homeLogin.js
+++ views/scripts/homeLogin.js
@@ -3,5 +3,11 @@ jQuery(document).ready(function($) {
 });
 
 function onSignIn(googleUser) {
-    document.location.href = "/lobby";
+    console.log(googleUser);
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+    // document.location.href = "/lobby";
   }
